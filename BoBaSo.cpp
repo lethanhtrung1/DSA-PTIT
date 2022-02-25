3810bee9fc03af320824d206f3f47fb054ab5c03
Trung <83465006+lethanhtrung1@users.noreply.github.com>
2022-02-26 02:13:43 +0700

    Add files via upload

---
+++ BoBaSo.cpp
@@ -0,0 +1,55 @@
+// Bo ba so tong bang k
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+int solution(ll a[], int n, ll k) {
+    for(int i = 0; i < n-2; i++) {
+        for(int j = i+1; j < n-1; j++) {
+            if((upper_bound(a+j+1, a+n, k-a[i]-a[j]) - a) != lower_bound(a+j+1, a+n, k-a[i]-a[j]) - a)
+                return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+    int test; cin >> test;
+    while(test--) {
+        int n; ll k;
+        cin >> n >> k;
+        ll a[n];
+        for(int i = 0; i < n; i++) cin >> a[i];
+        sort(a, a+n);
+        if(solution(a, n, k)) cout << "YES" << endl;
+        else cout << "NO" << endl;
+    }
+
+    return 0;
+}
+
+
+
+// Bo ba so tong nho hon k
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test; cin >> test;
+    while(test--) {
+        int n; long long k;
+        cin >> n >> k;
+        long long a[n];
+        for(int i = 0; i < n; i++) cin >> a[i];
+        sort(a, a+n);
+        long long res = 0;
+        for(int i = 0; i < n-2; i++) {
+            for(int j = i+1; j < n-1; j++) {
+                res += lower_bound(a+j+1, a+n, k-a[i]-a[j])-a-j-1;
+            }
+        }
+        cout << res << endl;
+    }
+
+    return 0;
+}
