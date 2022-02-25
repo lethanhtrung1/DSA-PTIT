f7483043aa12dba253459ad1efd222f1661dd685
Trung <83465006+lethanhtrung1@users.noreply.github.com>
2022-02-26 02:36:59 +0700

    Add files via upload

---
+++ CodePtit.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test; cin >> test;
+    while(test--) {
+        int n, k; cin >> n >> k;
+        long long a[n];
+        for(int i = 0; i < n; i++) cin >> a[i];
+        sort(a, a+n);
+        long long res = 0;
+        for(int i = 0; i < n-1; i++) {
+            res += (lower_bound(a+i+1, a+n, k+a[i])-a-i-1);
+        }
+        cout << res << endl;
+    }
+
+    return 0;
+}
