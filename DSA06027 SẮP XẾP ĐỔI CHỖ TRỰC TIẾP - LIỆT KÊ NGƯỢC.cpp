7224dcf42250d4de29af3947389fd018d08609c6
Trung <83465006+lethanhtrung1@users.noreply.github.com>
2022-02-28 00:32:20 +0700

    Update DSA06027 SẮP XẾP ĐỔI CHỖ TRỰC TIẾP - LIỆT KÊ NGƯỢC.cpp

--- DSA06027 SẮP XẾP ĐỔI CHỖ TRỰC TIẾP - LIỆT KÊ NGƯỢC.cpp
+++ DSA06027 SẮP XẾP ĐỔI CHỖ TRỰC TIẾP - LIỆT KÊ NGƯỢC.cpp
@@ -10,14 +10,11 @@ int main() {
         for(int &x : a) cin >> x;
         int cnt = 0;
         for(int i = 0; i <= n-1; i++) {
-            int check = 0;
             for(int j = i + 1; j < n; j++) {
                 if(a[i] > a[j]) {
                     swap(a[i], a[j]);
-                    check = 1;
                 }
             }
-            if(check == 0) break;
             cnt++;
             for(int j = 0; j < n; j++) {
                 b[i][j] = a[j];
