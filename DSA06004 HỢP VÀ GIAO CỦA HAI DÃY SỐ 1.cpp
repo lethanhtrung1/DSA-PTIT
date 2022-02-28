a3129ae9145b5904de46ebb224574e8bf9a15b36
Trung <83465006+lethanhtrung1@users.noreply.github.com>
2022-02-28 17:44:49 +0700

    Create DSA06004 HỢP VÀ GIAO CỦA HAI DÃY SỐ 1.cpp

---
+++ DSA06004 HỢP VÀ GIAO CỦA HAI DÃY SỐ 1.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test; cin >> test;
+    while(test--) {
+        int n, m; cin >> n >> m;
+        int a[n], b[m];
+        for(int &x : a) cin >> x;
+        for(int &x : b) cin >> x;
+        vector <int> Union, Intersection;
+        int i = 0, j = 0;
+        while(i < n && j < m) {
+            if(a[i] < b[j]) {
+                Union.push_back(a[i]);
+                i++;
+            }
+            else if(a[i] > b[j]) {
+                Union.push_back(b[j]);
+                j++;
+            }
+            else {
+                Union.push_back(a[i]);
+                Intersection.push_back(a[i]);
+                i++; j++;
+            }
+        }
+        while(i < n) Union.push_back(a[i++]);
+        while(j < m) Union.push_back(b[j++]);
+        for(int x : Union) cout << x << " ";
+        cout << endl;
+        for(int x : Intersection) cout << x << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
