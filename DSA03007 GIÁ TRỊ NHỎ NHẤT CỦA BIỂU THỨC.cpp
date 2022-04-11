ae3d6370f8dd2dde4f878f3e2eaa981a85c2c960
Trung <83465006+lethanhtrung1@users.noreply.github.com>
2022-04-11 23:27:49 +0700

    Create DSA03007 GIÁ TRỊ NHỎ NHẤT CỦA BIỂU THỨC.cpp

---
+++ DSA03007 GIÁ TRỊ NHỎ NHẤT CỦA BIỂU THỨC.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        ll a[n], b[n];
+        for(ll &x : a) cin >> x;
+        for(ll &x : b) cin >> x;
+        sort(a, a + n);
+        sort(b, b + n, greater<int>());
+        ll ans = 0;
+        for(int i = 0; i < n; i++) {
+            ans += a[i] * b[i];
+        }
+        cout << ans << endl;
+    }
+
+    return 0;
+}
